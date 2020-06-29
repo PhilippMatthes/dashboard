191eb5ceef04b78ae3724c4f7943258b74db8ed8
PhilippMatthes <github@philippmatth.es>
2020-06-29 23:52:39 +0200

    Add small bugfixes and improvements

--- app/assets/javascript/config.js
+++ app/assets/javascript/config.js
@@ -1,13 +1,20 @@
 
 class Location {
-    constructor(name, longitude, latitude) {
-        this.name = name;
-        this.longitude = longitude;
-        this.latitude = latitude;
-    }
+  constructor(name, longitude, latitude) {
+    this.name = name;
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
 }
 
-// TODO: Implement a better way of configuring the location
 const config = {
-    location: new Location('Dresden', 13.737262, 51.050407),
+  location: new Location('Dresden', 13.737262, 51.050407),
+  momentCalendarFormat: {
+    sameDay: '[Today] HH:mm',
+    nextDay: '[Tomorrow] HH:mm',
+    nextWeek: 'dddd',
+    lastDay: '[Yesterday] HH:mm',
+    lastWeek: '[Last] dddd',
+    sameElse: 'DD/MM/YYYY'
+  },
 };
